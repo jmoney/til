f4e267ad48f4f6e068b426110d94c06276550c63
Jonathan Monette <jon.monette@gmail.com>
2020-08-12 23:31:39 -0500

    Update to latest template

--- .template/find_markdown.py
+++ .template/find_markdown.py
@@ -3,8 +3,14 @@
 import glob
 import os
 import json
+import re
+
+regex = re.compile('\d{2}_\d{2}_\d{4}')
 
 def format_filename(filename):
+    match = re.match(regex, filename)
+    if match:
+        return os.path.splitext(filename)[0].replace("_", "/")    
     return os.path.splitext(filename)[0].replace("-", " ")
 
 def format_capital_filename(filename):
@@ -14,22 +20,23 @@ def format_title_filename(filename):
     return format_filename(filename).title()
 
 def build_markdown_file(markdown):
+    markdown_item = dict()
+
     markdown_file = dict()
     markdown_file["capital_filename"] = format_capital_filename(os.path.basename(markdown))
-    markdown_file["title_filename"] = format_title_filename(os.path.basename(markdown))
     markdown_file["fullpath"] = markdown
-    return markdown_file
+
+    markdown_item[format_title_filename(os.path.basename(markdown))] = markdown_file
+    return markdown_item
 
 if __name__ == '__main__':
     markdowns = dict()
 
     for markdown in glob.glob("./markdown/**/*.md", recursive=True):
         basepath = os.path.basename(os.path.dirname(markdown))
-        if basepath in markdowns:
-            markdown_file = build_markdown_file(markdown)
-            markdowns[basepath].append(markdown_file)
-        else:
-            markdown_file = build_markdown_file(markdown)
-            markdowns[basepath] = [markdown_file]
+        if basepath not in markdowns:
+            markdowns[basepath] = dict()
 
-    print(json.dumps(markdowns))
+        markdowns[basepath].update(build_markdown_file(markdown))
+
+    print(json.dumps(markdowns, sort_keys=True, indent=4))
