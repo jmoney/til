28eaaf6486e07cff29b01f56af35d85951abce97
Jonathan Monette <jon.monette@gmail.com>
2023-07-10 12:35:08 -0500

    migrate to gha

--- .template/readme/generate_data.py
+++ .template/readme/generate_data.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/opt/homebrew/bin/python3
 
 import glob
 import os
@@ -10,7 +10,7 @@ regex = re.compile('\d{2}_\d{2}_\d{4}')
 def format_filename(filename):
     match = re.match(regex, filename)
     if match:
-        return os.path.splitext(filename)[0].replace("_", "/")    
+        return os.path.splitext(filename)[0].replace("_", "/")
     return os.path.splitext(filename)[0].replace("-", " ")
 
 def format_capital_filename(filename):
@@ -20,23 +20,24 @@ def format_title_filename(filename):
     return format_filename(filename).title()
 
 def build_markdown_file(markdown):
-    markdown_item = dict()
-
     markdown_file = dict()
     markdown_file["capital_filename"] = format_capital_filename(os.path.basename(markdown))
+    markdown_file["title_filename"] = format_title_filename(os.path.basename(markdown))
     markdown_file["fullpath"] = markdown
-
-    markdown_item[format_title_filename(os.path.basename(markdown))] = markdown_file
-    return markdown_item
+    return markdown_file
 
 if __name__ == '__main__':
     markdowns = dict()
 
-    for markdown in glob.glob("./markdown/**/*.md", recursive=True):
+    for markdown in sorted(glob.glob("./markdown/**/*.md", recursive=True)):
         basepath = os.path.basename(os.path.dirname(markdown))
-        if basepath not in markdowns:
-            markdowns[basepath] = dict()
+        if basepath in markdowns:
+            markdown_file = build_markdown_file(markdown)
+            markdowns[basepath].append(markdown_file)
+        else:
+            markdown_file = build_markdown_file(markdown)
+            markdowns[basepath] = [markdown_file]
+    notes = dict()
+    notes["notes"] = markdowns
 
-        markdowns[basepath].update(build_markdown_file(markdown))
-
-    print(json.dumps(markdowns, sort_keys=True, indent=4))
+    print(json.dumps(notes, sort_keys=True, indent=4))
